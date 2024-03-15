484c8cb8d5629443027813d8dcc59af91d704133
kellyannclark <thefaithseed@gmail.com>
2024-03-15 14:51:54 -0600

    changes

--- week04/code/Node.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Node {
-    public int Data { get; set; }
-    public Node? Next { get; set; }
-    public Node? Prev { get; set; }
-
-    public Node(int data) {
-        this.Data = data;
-    }
-}
--- week05/code/TupleListExtensionMethods.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-
-public static class TupleListExtensionMethods {
-    public static string AsString(this IEnumerable list) {
-        return "<List>{" + string.Join(", ", list.Cast<ValueTuple<int, int>>()) + "}";
-    }
-}
